d4f162b6beac065eef5247747f88798d6eac05b1
Finn Stokes <finn.stokes@gmail.com>
2012-05-07 17:00:27 +0930

    Partway through changing event system

--- src/building.py
+++ src/building.py
@@ -1,30 +1,28 @@
 class Building:
-    def __init__(self, event):
-        self.event = event
-        event.register("input build", buildRoom)
-        event.register("input elevator", buildElevator)
+    def __init__(self, model):
+        self.model = model
     
     def update(self, dt):
         pass # update elevator position and room actions
     
-    def buildRoom(self, floor, room_id):
+    def build_room(self, floor, room_id):
         # construct new room at floor (check that this is next above or below)
-        self.event.notify("update room", floor, room_id)
+        self.model.dispatch_event('update_room', floor, room_id)
     
-    def buildElevator(self, left, floors):
+    def build_elevator(self, left, floors):
         # construct new elevator servicing given floors (on left if left is true, on right if false)
         # determine initial position
-        self.event.notify(left,floors,y)
+        self.model.dispatch_event('add_elevator', left,floors,y)
     
-    def getElevator(self, floor, left):
+    def get_elevator(self, floor, left):
         pass # get the elevator at floor (on left if left is true, on right if false)
 
 class Elevator:
     def __init__(self, floors, y):
         pass
     
-    def distanceFrom(floor):
+    def distance_from(floor):
         pass # calculate distance from given floor
     
-    def callTo(floor):
+    def call_to(floor):
         pass # add floor to queue of passenger pickups
