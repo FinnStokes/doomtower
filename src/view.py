bc9e7616f5d97dcf537076426223a824fb7f26cc
HelixWhiza <valkyrieavenger@gmail.com>
2012-05-07 21:43:42 +0930

    trying to get rendering working

--- src/view.py
+++ src/view.py
@@ -5,7 +5,7 @@ import input
 
 class View:
     def __init__(self, event_manager):
-        window = pyglet.window.Window()
+        window = pyglet.window.Window(800,600)
         
         self.render = render.Render(window, event_manager)
         self.input = input.Input(window, event_manager)
