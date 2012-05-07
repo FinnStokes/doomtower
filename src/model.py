aca4377c73823d1d0e5c38991bf1cc3f5390cc62
Finn Stokes <finn.stokes@gmail.com>
2012-05-07 21:08:53 +0930

    Moved event definitions into event.py

--- src/model.py
+++ src/model.py
@@ -5,7 +5,3 @@ import building
 class Model:
     def __init__(self, event_manager):
         self.building = building.Building(event_manager)
-
-event.Event.register_event_type('add_entity')
-event.Event.register_event_type('remove_entity')
-event.Event.register_event_type('update_entity')
