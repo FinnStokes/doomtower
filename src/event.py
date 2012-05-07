40de1157f054ca292a4bc11ba303bb563ab77344
Finn Stokes <finn.stokes@gmail.com>
2012-05-07 22:07:24 +0930

    Fixed error in event

--- src/event.py
+++ src/event.py
@@ -7,7 +7,7 @@ class Event(pyglet.event.EventDispatcher):
 # This event should be produced when a new room gets built, a room is destrpyed (setting id to 0)
 # or a room is remodelled.
 # Render should react by changing the graphic accordingly.
-event.Event.register_event_type('update_room')
+Event.register_event_type('update_room')
 
 # new_elevator(id, left, floors, y)
 # This event should be produced when a new elevator is added.
@@ -16,17 +16,17 @@ event.Event.register_event_type('update_room')
 #  The set floors should indicate what floors the elevator services.
 #  The float y should indicate what height the elevator should start at.
 # Render should react by adding the relevant graphic.
-event.Event.register_event_type('new_elevator')
+Event.register_event_type('new_elevator')
 
 # remove_elevator(id)
 # This event should be produced when an elevator is removed.
 # Render should react by deleting the relevant graphic.
-event.Event.register_event_type('remove_elevator')
+Event.register_event_type('remove_elevator')
 
 # update_elevator(id, y)
 # This event should be produced when an elevator changes position.
 # Render should react by changing the elevator graphic to reflect this.
-event.Event.register_event_type('update_elevator')
+Event.register_event_type('update_elevator')
 
 # new_entity(id, x, y)
 # This event should be produced when a new entity enters the building.
@@ -34,12 +34,12 @@ event.Event.register_event_type('update_elevator')
 #  The float x should indicate how far across the building the entity is (0 = left wall, 1 = right wall).
 #  The integer y should indicate what floor the entity is on.
 # Render should react by adding the relevant graphic.
-event.Event.register_event_type('new_entity')
+Event.register_event_type('new_entity')
 
 # remove_entity(id)
 # This event should be produced when an entity is removed.
 # Render should react by deleting the relevant graphic.
-event.Event.register_event_type('remove_entity')
+Event.register_event_type('remove_entity')
 
 # update_entity(id, x, y)
 # This event should be produced when an entity changes position.
@@ -47,4 +47,4 @@ event.Event.register_event_type('remove_entity')
 #  The float x should indicate how far across the building the entity is (0 = left wall, 1 = right wall).
 #  The integer y should indicate what floor the entity is on.
 # Render should react by moving the entity graphic.
-event.Event.register_event_type('update_entity')
+Event.register_event_type('update_entity')
