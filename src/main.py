f313dd3de2cf78ba3b6e637e79231eae13e2aa19
Saxon Douglass <saxon.douglass@gmail.com>
2012-05-13 02:56:21 +0930

    Removed starting client and set first client to spawn more rapidly

--- src/main.py
+++ src/main.py
@@ -13,8 +13,6 @@ pygame.init()
 fpsClock = pygame.time.Clock()
 running = True
 
-event_manager.notify("create_client")
-
 event_manager.notify("input_elevator",True,[-4, -2, 0, 2, 4, 6, 8, 10])
 event_manager.notify("input_elevator",False,[-5, -4])
 event_manager.notify("input_elevator",False,[-3, -2])
@@ -29,7 +27,7 @@ def close():
     running = False
 
 event_manager.register("quit", close)
-accum = 0.0
+accum = settings.SPAWN_PERIOD_END
 total = 0
 
 while running:
